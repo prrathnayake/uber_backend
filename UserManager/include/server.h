451ebc6d5a63cddc4fabdab36d159b7c1b18e335
prrathnayake <pasanransika1@gmail.com>
2025-06-25 10:33:53 +1000

    fixing small issues

--- UserManager/include/server.h
+++ UserManager/include/server.h
@@ -4,17 +4,19 @@
 #include <utils/index.h>
 #include <database/database.h>
 
-namespace uber_backend {
+namespace uber_backend
+{
 
-class Server {
-public:
-    Server();
-    void initiateDatabase();
+    class Server
+    {
+    public:
+        Server();
+        void initiateDatabase();
 
-private:
-    std::unique_ptr<uber_backend::uber_database> database_;
-    std::unique_ptr<utils::ThreadPool> thread_pool_;
-    // Remove this: std::unique_ptr<utils::FileLogger> fileLogger_;
+    private:
+        std::unique_ptr<uber_backend::uber_database> database_;
+        std::unique_ptr<utils::ThreadPool> thread_pool_;
+        utils::SingletonLogger &logger_;
+    };
+
 };
-
-}
--- UserManager/src/server.cpp
+++ UserManager/src/server.cpp
@@ -1,26 +1,25 @@
+#include <iostream>
+
+#include <utils/index.h>
 #include "../include/server.h"
-#include "utils/index.h" // Adjust path if needed
+
 
 using namespace utils;
 using namespace uber_backend;
 
-Server::Server()
+Server::Server() : logger_(SingletonLogger::instance()) // initialize reference once
 {
-    SingletonLogger &logger = SingletonLogger::instance();
     thread_pool_ = std::make_unique<ThreadPool>(64);
-
-    logger.logMeta(SingletonLogger::INFO, "Server initialized", __FILE__, __LINE__, __func__);
+    logger_.logMeta(SingletonLogger::INFO, "Server initialized", __FILE__, __LINE__, __func__);
 }
 
 void Server::initiateDatabase()
 {
-    SingletonLogger &logger = SingletonLogger::instance();
-
     auto result = thread_pool_->enqueue([](int x)
                                         { return x * x; }, 8);
 
-    logger.logMeta(SingletonLogger::ERROR, "Creating database instance.", __FILE__, __LINE__, __func__);
+    logger_.logMeta(SingletonLogger::ERROR, "Creating database instance.", __FILE__, __LINE__, __func__);
 
     database_ = std::make_unique<uber_database>();
-    database_->initalizeDatabase(); // Pass logger/config if needed
+    database_->initalizeDatabase();
 }
